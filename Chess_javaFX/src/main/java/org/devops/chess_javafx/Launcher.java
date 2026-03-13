e85822e840c6b1df5fbf2ced967a9924c4f48ea7
Quentin Porte <142680189+Quentdeux@users.noreply.github.com>
2026-03-13 10:03:21 +0100

    Ajout fichiers manquants

---
+++ Chess_javaFX/src/main/java/org/devops/chess_javafx/Launcher.java
@@ -0,0 +1,9 @@
+package org.devops.chess_javafx;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
